e0da9c6d32b9e5684ce3e007c655e23fabb4443c
David Tai <redfrog999@users.noreply.github.com>
2023-03-16 20:09:37 +0800

    Update diy-part2.sh

--- ramips-hnat/diy-part2.sh
+++ ramips-hnat/diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP 默认IP由0.1修改为1.1
 sed -i 's/192.168.0.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+
+# 最大连接数修改为65535
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
