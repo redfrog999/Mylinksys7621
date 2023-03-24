9989003b2f6b10a4ffcbdeee966a58dd6c111267
David Tai <redfrog999@users.noreply.github.com>
2023-03-25 00:54:12 +0800

    Update diy-part1.sh

--- ramips-hnat/diy-part1.sh
+++ ramips-hnat/diy-part1.sh
@@ -14,7 +14,7 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git adguardhome https://github.com/AdguardTeam/AdGuardHome' >>feeds.conf.default
 
