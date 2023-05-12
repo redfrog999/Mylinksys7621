6d9709282cf65ca89f654669ce69d2eeab5113e6
David Tai <redfrog999@users.noreply.github.com>
2023-05-12 10:43:51 +0800

    Update diy-part2.sh

--- ramips-hnat/diy-part2.sh
+++ ramips-hnat/diy-part2.sh
@@ -17,5 +17,5 @@ sed -i 's/192.168.0.1/192.168.1.1/g' package/base-files/files/bin/config_generat
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 #
 
-rm -rf package/passwall
-git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
+#rm -rf package/passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
