caebf84450cfefdbba1420b1f18e1f6c50ed8a4d
David Tai <redfrog999@users.noreply.github.com>
2023-05-12 16:42:19 +0800

    Update diy-part1.sh

--- ramips-hnat/diy-part1.sh
+++ ramips-hnat/diy-part1.sh
@@ -15,8 +15,6 @@
 
 
 # Add a feed source
-# echo 'src-git packages https://github.com/immortalwrt/packages.git' >>feeds.conf.default
-# echo 'src-git luci https://github.com/immortalwrt/luci.git' >>feeds.conf.default
 # echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git adguardhome https://github.com/AdguardTeam/AdGuardHome.git' >>feeds.conf.default
@@ -24,6 +22,5 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.co
 
 # 添加插件源码
 sed -i '$a src-git-full kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git-full packages https://github.com/immortalwrt/packages.git' feeds.conf.default
-# sed -i '$a src-git-full luci https://github.com/immortalwrt/luci.git' feeds.conf.default
+
 
