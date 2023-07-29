a62755af79fb2ccc7c9e907cc50179ff973f612b
David Tai <redfrog999@users.noreply.github.com>
2023-07-30 01:45:14 +0800

    Update diy-part1.sh

--- ramips-hnat/diy-part1.sh
+++ ramips-hnat/diy-part1.sh
@@ -17,10 +17,12 @@ echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf
 # Add a feed source
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
 echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
+echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
 # echo 'src-git adguardhome https://github.com/AdguardTeam/AdGuardHome.git' >>feeds.conf.default
 # echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 
 # 添加插件源码
 sed -i '$a src-git-full kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 
