b552d06346888d35640102bc3595d35e21acbb0c
David Tai <redfrog999@users.noreply.github.com>
2023-09-29 16:34:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,8 @@
 #echo 'src-git-full packages https://github.com/immortalwrt/packages.git' >>feeds.conf.default
 #echo 'src-git-full luci https://github.com/immortalwrt/luci.git' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
 # echo 'src-git adguardhome https://github.com/AdguardTeam/AdGuardHome.git' >>feeds.conf.default
 # echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
