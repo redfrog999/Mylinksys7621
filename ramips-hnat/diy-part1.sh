876aaf848b1bf97a976b008f5014afb3f5f16305
David Tai <redfrog999@users.noreply.github.com>
2023-09-29 16:49:09 +0800

    Update diy-part1.sh

--- ramips-hnat/diy-part1.sh
+++ ramips-hnat/diy-part1.sh
@@ -20,7 +20,7 @@ echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >
 echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
 
 # 添加插件源码
-sed -i '$a src-git-full kenzo https://github.com/redfrog99/kenzok8-openwrt-packages' feeds.conf.default
+#sed -i '$a src-git-full kenzo https://github.com/RavenCage/kenzok8-openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 
