311addbb8f27bdb9daf46b4c5b33915bcf6b4b39
David Tai <redfrog999@users.noreply.github.com>
2024-02-28 19:58:27 +0800

    Update diy-part1-1.sh

--- ramips-hnat/diy-part1-1.sh
+++ ramips-hnat/diy-part1-1.sh
@@ -11,8 +11,8 @@
 #
 
 # Uncomment a feed source
-sed -i "/helloworld/d" "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+#sed -i "/helloworld/d" "feeds.conf.default"
+#echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 
 # Add a feed source
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
