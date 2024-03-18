33d33b3299c4bb7992926a489f0a9b1fe3a08054
David Tai <redfrog999@users.noreply.github.com>
2024-03-19 01:55:16 +0800

    Update diy-part2.sh

--- ramips-hnat/diy-part2.sh
+++ ramips-hnat/diy-part2.sh
@@ -10,8 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP 默认IP由0.1修改为1.1
-sed -i 's/192.168.0.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+# Modify default IP 
+sed -i 's/192.168.1.1/192.168.68.1/g' package/base-files/files/bin/config_generate
 
 # 最大连接数修改为65535
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
