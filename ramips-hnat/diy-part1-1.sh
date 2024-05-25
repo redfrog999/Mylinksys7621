234a45c4b989f5387ea0066dd5161098bc56ca80
David Tai <redfrog999@users.noreply.github.com>
2024-05-25 13:27:01 +0800

    Update diy-part1-1.sh

--- ramips-hnat/diy-part1-1.sh
+++ ramips-hnat/diy-part1-1.sh
@@ -15,10 +15,10 @@
 #echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 
 # Add a feed source
-echo "src-git passwall_packages https://github.com/RavenCage/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/redfrog999/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-dev" >> "feeds.conf.default"
 
 # 添加插件源码
-sed -i '$a src-git-full kenzo https://github.com/RavenCage/kenzok8-openwrt-packages' feeds.conf.default
+sed -i '$a src-git-full kenzo https://github.com/kenzok8/kenzok8-openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
