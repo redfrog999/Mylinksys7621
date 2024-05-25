d2bf49649550bd03cbc6740b6bed57e89152d5d5
David Tai <redfrog999@users.noreply.github.com>
2024-05-25 13:29:36 +0800

    Update diy-part2-2.sh

--- ramips-hnat/diy-part2-2.sh
+++ ramips-hnat/diy-part2-2.sh
@@ -21,7 +21,6 @@ find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/RavenCage/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 echo '替换golang到1.22.x'
