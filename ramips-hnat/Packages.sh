2f764635a65c4ee50e86cb6e9c72d9b9b996d1ff
David Tai <redfrog999@users.noreply.github.com>
2024-09-13 15:14:42 +0800

    Update Packages.sh

--- ramips-hnat/Packages.sh
+++ ramips-hnat/Packages.sh
@@ -27,7 +27,7 @@ UPDATE_PACKAGE "design-config" "gngpp/luci-app-design-config" "master"
 UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
 UPDATE_PACKAGE "argon-config" "jerrykuku/luci-app-argon-config" "master"
 UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
-
+UPDATE_PACKAGE "mihomo" "morytyann/OpenWrt-mihomo" "main"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "master"
 
 #更新软件包版本
