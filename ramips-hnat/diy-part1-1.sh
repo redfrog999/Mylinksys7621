a75fbb9de688026ba662499102d41057f59c94b1
David Tai <redfrog999@users.noreply.github.com>
2024-09-15 07:03:24 +0800

    Update diy-part1-1.sh

--- ramips-hnat/diy-part1-1.sh
+++ ramips-hnat/diy-part1-1.sh
@@ -20,8 +20,8 @@ echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;luci-sm
 #echo "src-git mihomo https://github.com/redfrog999/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
 
 # 添加插件源码
-sed -i '$a src-git-full kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git kiddin9 https://github.com/redfrog999/kiddin9-openwrt-packages' feeds.conf.default
+#sed -i '$a src-git-full kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/redfrog999/kiddin9-openwrt-packages' feeds.conf.default
 #sed -i 's/+firewall/+uci-firewall/g' feeds/luci/applications/luci-app-firewall/Makefile
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
