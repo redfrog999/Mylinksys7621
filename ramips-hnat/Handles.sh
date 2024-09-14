ba2f803d93d1c8d426e0b0864e2c45b7604b22d9
David Tai <redfrog999@users.noreply.github.com>
2024-09-14 16:57:27 +0800

    Update Handles.sh

--- ramips-hnat/Handles.sh
+++ ramips-hnat/Handles.sh
@@ -5,13 +5,16 @@ PKG_PATCH="$GITHUB_WORKSPACE/wrt/package/"
 # mihomo
 if [ -d *"mihomo"* ]; then
         git clone https://github.com/redfrog999/OpenWrt-mihomo  package/OpenWrt-mihomo
-        mkdir -p files/etc/mihomo/run/ui
+
+        cd ./Openwrt-mihomo/luci-app-mihomo/root/etc/mihomo/
         
-        curl -Lso files/etc/mihomo/run/Country.mmdb https://github.com/NobyDa/geoip/raw/release/Private-GeoIP-CN.mmdb
-        curl -Lso files/etc/mihomo/run/GeoIP.dat https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat
-        curl -Lso files/etc/mihomo/run/GeoSite.dat https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat
+        mkdir -p root/etc/mihomo/run/ui
+        
+        curl -Lso root/etc/mihomo/run/Country.mmdb https://github.com/NobyDa/geoip/raw/release/Private-GeoIP-CN.mmdb
+        curl -Lso root/etc/mihomo/run/GeoIP.dat https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat
+        curl -Lso root/etc/mihomo/run/GeoSite.dat https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat
         curl -Lso metacubexd-gh-pages.tar.gz https://github.com/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.tar.gz
         
         tar zxf metacubexd-gh-pages.tar.gz
-        mv metacubexd-gh-pages files/etc/mihomo/run/ui/metacubexd     
+        mv metacubexd-gh-pages root/etc/mihomo/run/ui/metacubexd     
 fi
