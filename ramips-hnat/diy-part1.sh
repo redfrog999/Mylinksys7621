01532453da51298e7ef99599ca55f6aa925f913f
David Tai <redfrog999@users.noreply.github.com>
2024-09-25 20:59:14 +0800

    Update diy-part1.sh

--- ramips-hnat/diy-part1.sh
+++ ramips-hnat/diy-part1.sh
@@ -15,9 +15,9 @@
 #echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 
 # Add a feed source
-#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-dev" >> "feeds.conf.default"
-#echo "src-git mihomo https://github.com/redfrog999/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-dev" >> "feeds.conf.default"
+echo "src-git mihomo https://github.com/redfrog999/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
 
 # 添加插件源码
 sed -i '$a src-git-full kenzo https://github.com/redfrog999/openwrt-packages' feeds.conf.default
