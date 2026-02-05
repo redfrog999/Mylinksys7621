2c5f499e84c1376f13af51f014dc82842a50fa61
David Tai <redfrog999@users.noreply.github.com>
2026-02-05 15:08:31 +0800

    Modify sysctl.conf and set permissions for uci-defaults
    Added permission change for uci-defaults script to prevent loss of execution rights during cloud compilation.

--- ramips-hnat/diy-part2-2.sh
+++ ramips-hnat/diy-part2-2.sh
@@ -16,6 +16,9 @@ sed -i 's/192.168.0.1/192.168.1.1/g' package/base-files/files/bin/config_generat
 # 最大连接数修改为65535
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
+# 強制給予 uci-defaults 腳本執行權限，防止雲端編譯權限丟失
+chmod +x files/etc/uci-defaults/99-physical-sovereignty
+
 # golang 1.25
 rm -rf feeds/packages/lang/golang
 git clone --depth=1 https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
