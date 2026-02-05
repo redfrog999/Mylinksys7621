7b57d38f1d1d680fab86d0dbeb59fda8ba97b3fe
David Tai <redfrog999@users.noreply.github.com>
2026-02-05 15:08:49 +0800

    Update diy-part2.sh

--- ramips-hnat/diy-part2.sh
+++ ramips-hnat/diy-part2.sh
@@ -16,6 +16,9 @@ sed -i 's/192.168.1.1/192.168.68.1/g' package/base-files/files/bin/config_genera
 # 最大连接数修改为65535
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
+# 強制給予 uci-defaults 腳本執行權限，防止雲端編譯權限丟失
+chmod +x files/etc/uci-defaults/99-physical-sovereignty
+
 echo '替换golang到1.24x'
 rm -rf feeds/packages/lang/golang
 git clone -b 24.x --single-branch https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
