7f7a841f3c4d68b8950ea9ba9adc0cec6631ba84
David Tai <redfrog999@users.noreply.github.com>
2026-02-19 22:12:27 +0800

    Update diy-part2-2.sh

--- ramips-hnat/diy-part2-2.sh
+++ ramips-hnat/diy-part2-2.sh
@@ -10,29 +10,32 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP 默认IP由0.1修改为1.1
-sed -i 's/192.168.0.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+# Modify default IP 
+sed -i 's/192.168.1.1/10.0.10.1/g' package/base-files/files/bin/config_generate
 
 # 最大连接数修改为65535
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
+# ------------------PassWall 彻底清除--------------------------
+# 移除 openwrt feeds 自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,pdnsd-alt,chinadns-ng,dns2socks,dns2tcp,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview}
+# 核心库
+rm -rf package/passwall-packages/{shadowsocks-rust,v2ray-geodata}
+# app
+rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-ssr-libev-server}
+
 # 強制給予 uci-defaults 腳本執行權限，防止雲端編譯權限丟失
 chmod +x files/etc/uci-defaults/99-physical-sovereignty
 
-# golang 1.26
+echo '替换golang到1.26x'
 rm -rf feeds/packages/lang/golang
-git clone --depth=1 https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+git clone -b 26.x --single-branch https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 echo '=========Replace golang OK!========='
 
-# 主题设置
-# git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
-# echo "CONFIG_PACKAGE_luci-theme-aurora=y" >> .config
+#致敬老兵，让MT7621在ShellClash的加持下重新焕发战斗神采
 
-# 取消自添加主题的默认设置
-# find package/luci-theme-*/* -type f -print | grep '/root/etc/uci-defaults/' | while IFS= read -r file; do
-	# sed -i '/set luci.main.mediaurlbase/d' "$file"
-# done
+# 1. 强制重写全局编译参数，针对 MT7621 (MIPS 1004Kc/74kc) 深度优化
+sed -i 's/-O2/-O3 -march=mipsel -mtune=74kc -mdsp -mno-mips16 -fno-caller-saves/g' include/target.mk
 
-# 设置默认主题
-# default_theme='aurora'
-# sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
+# 2. 优化特定的编译器标志，减少延迟
+sed -i 's/TARGET_CFLAGS:=/TARGET_CFLAGS:=-O3 -march=mipsel -mtune=74kc -mdsp -mno-mips16 -falign-functions=32 -fno-caller-saves -fomit-frame-pointer -pipe /g' include/target.mk
