b294964f41d66443e4fba6d9986f3bb867c1a4d4
David Tai <redfrog999@users.noreply.github.com>
2026-02-20 14:28:56 +0800

    Update diy-part2-2.sh

--- ramips-hnat/diy-part2-2.sh
+++ ramips-hnat/diy-part2-2.sh
@@ -16,23 +16,33 @@ sed -i 's/192.168.1.1/10.0.10.1/g' package/base-files/files/bin/config_generate
 # 最大连接数修改为65535
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
-# ------------------PassWall 彻底清除--------------------------
-# 移除 openwrt feeds 自带的核心库
-# rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,pdnsd-alt,chinadns-ng,dns2socks,dns2tcp,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview}
-# 核心库
-# rm -rf package/passwall-packages/{shadowsocks-rust,v2ray-geodata}
-# app
-# rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-ssr-libev-server}
+# ------------------PassWall 重构MRS版--------------------------
+# 1. 创建固化规则存放路径
+mkdir -p files/etc/xray/rules/
+# 2. 从可靠源下载预编译的二进制 MRS 文件 (这里以精简版为例)
+wget ${https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/gfw.mrs}/gfw.mrs -O files/etc/xray/rules/gfw.mrs
+wget ${https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/google.mrs}/google.mrs -O files/etc/xray/rules/google.mrs
+wget ${https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/youtube.mrs}/youtube.mrs -O files/etc/xray/rules/youtube.mrs
+wget ${https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/telegram.mrs}/telegram.mrs -O files/etc/xray/rules/telegram.mrs
+wget ${https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/netflix.mrs}/netflix.mrs -O files/etc/xray/rules/netflix.mrs
+wget ${https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/category-ai-!cn.mrs}/ai.mrs -O files/etc/xray/rules/ai.mrs
+wget ${https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/github.mrs}/github.mrs -O files/etc/xray/rules/github.mrs
+wget ${https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/reddit.mrs}/reddit.mrs -O files/etc/xray/rules/reddit.mrs
+wget ${https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/twitter.mrs}/twitter.mrs -O files/etc/xray/rules/twitter.mrs
+# 下载大陆 IP 段 (MRS 格式)
+wget https://github.com/v2fly/geoip/releases/latest/download/cn.mrs -O files/etc/xray/rules/china_ip.mrs
+wget https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/cn.mrs
 
-# 強制給予 uci-defaults 腳本執行權限，防止雲端編譯權限丟失
+# -----------------強制給予 uci-defaults 腳本執行權限，防止雲端編譯權限丟失-------------------------
 chmod +x files/etc/uci-defaults/99-physical-sovereignty
 
+#------------------替换GoLang-----------------------
 echo '替换golang到1.26x'
 rm -rf feeds/packages/lang/golang
 git clone -b 26.x --single-branch https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 echo '=========Replace golang OK!========='
 
-#致敬老兵，让MT7621在ShellClash的加持下重新焕发战斗神采
+#------------------致敬老兵，让MT7621在ShellClash的加持下重新焕发战斗神采---------------------
 
 # 1. 强制重写全局编译参数，针对 MT7621 (MIPS 1004Kc/74kc) 深度优化
 sed -i 's/-O2/-O3 -march=mipsel -mtune=74kc -mdsp -mno-mips16 -fno-caller-saves/g' include/target.mk
